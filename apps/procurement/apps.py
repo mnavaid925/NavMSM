71d8fb5ddc33d881878b53f6c937b06f6ea1e76b
Navaid-KloneIT <158593884+Navaid-KloneIT@users.noreply.github.com>
2026-05-04 01:43:19 +0500

    feat(procurement): AppConfig wiring signals on ready()

---
+++ apps/procurement/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class ProcurementConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.procurement'
+    verbose_name = 'Procurement & Supplier Portal'
+
+    def ready(self):
+        from . import signals  # noqa: F401
